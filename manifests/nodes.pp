587b8739fd6bdadb2f4c96b3221093f660c42943
Eric Drechsel <eric@sqrly.com>
2010-08-13 20:17:41 +0000

    amend

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -8,14 +8,11 @@ node 'sqrly' inherits default {
     include postgresql::administration
     include postgis
     include python
-
-    exec { "easy_install pip":
-	creates => "/usr/local/bin/pip",
-    }
-    exec { "pip install virtualenv":
-	creates => "/usr/local/bin/virtualenv",
-	require => exec["easy_install pip"]
-    }
+    include python::egg_virtualenv
+    #include django::stack
+    # make django::postgres, postgis classes, defines
+    django::app{ "sqrly": }
+     
     # include nginx
     # include user::sqrly
 
@@ -23,22 +20,10 @@ node 'sqrly' inherits default {
     # TODO: maybe create users for devs
 
     postgis::database{ "sqrly":
-	owner	=> sqrly
-    }
-
-    user { "sqrly":
-	uid	=> 2001,
-	home	=> '/srv/sqrly', #???
+        owner   => sqrly
     }
 
     postgresql::user{"sqrly":
-    }
-
-    file { "/srv/sqrly":
-	ensure => present,
-	owner => "sqrly",
-	group => "sqrly",
-	mode => 771,
     }
 }
 
