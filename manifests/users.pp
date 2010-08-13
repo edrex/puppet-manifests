5d52b523752d930585ff296100e73ab80a74cc85
Eric Drechsel <eric@sqrly.com>
2010-08-13 20:16:44 +0000

    manifests/nodes.pp

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,33 +1,4 @@
-define realuser($uid, $groups=[], $comment, $key, $keytype="ssh-rsa") {
-    group { $name :
-	gid	=> $uid,
-    }
 
-    user { $name :
-	uid	=> $uid,
-	gid	=> $uid,
-	groups  => $groups,
-	shell   => "/bin/bash",
-	comment => $comment,
-	home	=> "/home/$name",
-	managehome => true,
-	require => Group[$name],
-    }
-    
-    ssh_authorized_key { $name :
-	user	=> $name,
-	key	=> $key,
-	type	=> $keytype,
-    }
-
-    # require password change on first login
-    # WARNING: will unlock locked accounts, TODO fix that
-    exec { "usermod -p '' $name; chage -d 0 $name":
-        onlyif	=> "test `passwd -S $name|cut -d ' ' -f 2` == L",
-	require => User[$name],
-    }
-}
-	
 class user::virtual {
     @realuser { 
     "chris":
